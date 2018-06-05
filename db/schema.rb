f5376ddd77db79bef89476c07ea35ec5cad56643
Justine <jjtroyke@gmail.com>
2018-06-05 10:52:32 -0600

    add movie rating

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180605162114) do
+ActiveRecord::Schema.define(version: 20180605165115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20180605162114) do
     t.datetime "updated_at", null: false
     t.bigint "director_id"
     t.string "slug"
+    t.integer "rating"
     t.index ["director_id"], name: "index_movies_on_director_id"
   end
 
--- spec/features/visitor/visitor_movie_show_has_genres_spec.rb
+++ spec/features/visitor/visitor_movie_show_has_genres_spec.rb
@@ -19,13 +19,31 @@ describe "user visits movie show page" do
     expect(page).to have_content(genre.name)
     expect(page).to have_content(genre2.name)
   end
+
+  describe "user visits movie show page" do
+    it "visitor sees movie rating" do
+      name = 'Horror'
+      name2 = 'Comedy'
+      descrb2 = 'Best Don Knotts film'
+      director = Director.create(name: 'So and So')
+      movie2 = 'The Ghost and Mr. Chicken'
+      genre = Genre.create(name: name)
+      genre2 = Genre.create(name: name2)
+      movie = Movie.create(title: movie2, description: descrb2, director_id: director.id, rating: 4)
+      movie_genre = MovieGenre.create(movie_id: movie.id, genre_id: genre.id)
+      movie_genre2 = MovieGenre.create(movie_id: movie.id, genre_id: genre2.id)
+
+      visit movie_path(movie.slug)
+
+      expect(page).to have_content("Rating:")
+      expect(page).to have_content(movie.rating)
+    end
+  end
 end
+#
 # As a Visitor,
 #   When I visit a movie show page,
-#     I see genre names under a heading that says
-#     "Genres for this Movie:"
-#       and the genres listed are only the genres
-#       associated with this movie.
+#     I see the number rating for this movie
 #
-# (for testing purposes,
-#   I should see at least 2 genres listed for a movie)
+# (the rating should be an integer attribute (from 1 to 5)
+# for the movie)
